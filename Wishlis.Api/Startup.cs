c7f712fb3d7a57c4a208a2acbdbd60b92a9c2215
Danila Nikolaev <danila_nikolaev@epam.com>
2024-08-02 16:15:44 +0400

    add serverless api - AWS lambda behild API Gateway

---
+++ Wishlis.Api/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Wishlis.Api.Swagger;
+using Wishlis.Application.Mappers;
+using Wishlis.Application.Services;
+using Wishlis.Domain.Repositories;
+using Wishlis.Infrastructure.LiteDB;
+
+namespace Wishlis.Api;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddControllers();
+        services.AddAndConfigureSwagger();
+
+        // services
+        services.AddAutoMapper(typeof(MappingProfile));
+        services.AddTransient<IPersonService, PersonService>();
+        services.AddTransient<IWishlistItemService, WishlistItemService>();
+
+        // infrastructure
+        var n = Configuration.GetChildren().ToList();
+        services.Configure<LiteDbOptions>(Configuration.GetSection(nameof(LiteDbOptions)));
+        services.AddScoped<ILiteDbContext, LiteDbContext>();
+        services.AddScoped<IPersonRepository, PersonRepository>();
+        services.AddScoped<IWishlistItemRepository, WishlistItemRepository>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                var apiVersionDescriptionProvider = app.ApplicationServices.GetRequiredService<IApiVersionDescriptionProvider>();
+        
+                foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint(
+                        $"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant()); 
+                } 
+            });
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseEndpoints(x => x.MapControllers());
+    }
+}
---
+++ Wishlis.Serverless.Api/src/Wishlis.Serverless.Api/LocalEntryPoint.cs
@@ -0,0 +1,26 @@
+﻿using Wishlis.Api;
+
+namespace Wishlis.Serverless.Api;
+
+/// <summary>
+/// The Main function can be used to run the ASP.NET Core application locally using the Kestrel webserver.
+/// </summary>
+public class LocalEntryPoint
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                config.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
