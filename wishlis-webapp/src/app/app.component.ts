2c642a4370aab73d8903ca043ae8963ba671eb62
dartungar <dartungar@gmail.com>
2024-09-13 21:10:36 +0400

    Add AWS Amplify Authenticator component v0
    Add POC for authenticator component.
    Configuration still doesn't work though.

--- wishlis-webapp/src/app/app.component.ts
+++ wishlis-webapp/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {AmplifyAuthenticatorModule} from "@aws-amplify/ui-angular";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    AmplifyAuthenticatorModule
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wishlis-webapp';
+
 }
