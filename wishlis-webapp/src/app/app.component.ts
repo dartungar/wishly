448c432b2b9a67ef1016fdbf818a40ac92dc9636
dartungar <dartungar@gmail.com>
2024-09-18 17:31:58 +0400

    Finis basic Cognito auth implementation

--- wishlis-webapp/src/app/app.component.ts
+++ wishlis-webapp/src/app/app.component.ts
@@ -1,5 +1,41 @@
 import { Component } from '@angular/core';
 import {AmplifyAuthenticatorModule} from "@aws-amplify/ui-angular";
+import {Amplify} from "aws-amplify";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "us-east-1_pDllIs8gd",
+      userPoolClientId: "3hsne5em3e5ga0nsvku1ba1b25",
+      identityPoolId: "",
+      loginWith: {
+        email: true,
+        oauth: {
+          domain: "https://wishlis.auth.us-east-1.amazoncognito.com",
+          scopes: [],
+          redirectSignIn: ["https://localhost"],
+          redirectSignOut: ["https://localhost"],
+          responseType: 'token',
+          providers: ["Google", "Facebook"]
+        }
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+});
 
 @Component({
   selector: 'app-root',
