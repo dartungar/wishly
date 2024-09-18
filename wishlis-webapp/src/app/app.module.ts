1804e194c27067bcfda901dd38f371ffb84974b2
dartungar <dartungar@gmail.com>
2024-09-18 17:31:58 +0400

    Finis basic Cognito auth implementation

--- wishlis-webapp/src/app/app.module.ts
+++ wishlis-webapp/src/app/app.module.ts
@@ -1,45 +1,8 @@
 import { NgModule} from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {AmplifyAuthenticatorModule, AuthenticatorComponent} from '@aws-amplify/ui-angular';
-import {Amplify} from "aws-amplify";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/compiler";
-
-
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: "us-east-1_pDllIs8gd",
-      userPoolClientId: "7hlufeo473990p368hkh48pv1t",
-      identityPoolId: "<your-cognito-identity-pool-id>",
-      loginWith: {
-        oauth: {
-          domain: "https://wishlis.auth.us-east-1.amazoncognito.com",
-          scopes: [],
-          redirectSignIn: ["https://localhost"],
-          redirectSignOut: ["https://localhost"],
-          responseType: 'token',
-          providers: ["Google", "Facebook"]
-        }
-      },
-      signUpVerificationMethod: "code",
-      userAttributes: {
-        email: {
-          required: true,
-        },
-      },
-      allowGuestAccess: true,
-      passwordFormat: {
-        minLength: 8,
-        requireLowercase: true,
-        requireUppercase: true,
-        requireNumbers: true,
-        requireSpecialCharacters: true,
-      },
-    },
-  },
-});
 
 @NgModule({
   declarations: [],
