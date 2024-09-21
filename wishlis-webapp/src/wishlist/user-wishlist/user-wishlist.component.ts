8ab530f541525c88878f2216c7de02085d16cf88
dartungar <dartungar@gmail.com>
2024-09-21 14:21:09 +0400

    Add Angular components

---
+++ wishlis-webapp/src/wishlist/user-wishlist/user-wishlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-wishlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-wishlist.component.html',
+  styleUrl: './user-wishlist.component.css'
+})
+export class UserWishlistComponent {
+
+}
---
+++ wishlis-webapp/src/wishlist/wishlist-item/wishlist-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wishlist-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './wishlist-item.component.html',
+  styleUrl: './wishlist-item.component.css'
+})
+export class WishlistItemComponent {
+
+}
