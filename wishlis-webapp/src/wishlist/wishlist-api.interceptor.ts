29cfdca93f2d45e3a4c11f264f06880d9abb3e2b
dartungar <dartungar@gmail.com>
2024-10-13 16:07:27 +0400

    Add api interceptor stub

---
+++ wishlis-webapp/src/wishlist/wishlist-api.interceptor.ts
@@ -0,0 +1,14 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import {inject} from "@angular/core";
+import {AuthService} from "../auth/auth.service";
+
+export const wishlistApiInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = inject(AuthService).userToken;
+  if (token === undefined || token === null) {
+    return next(req);
+  }
+  const newReq = req.clone({
+    headers: req.headers.append('X-Authentication-Token', token),
+  });
+  return next(newReq);
+};
