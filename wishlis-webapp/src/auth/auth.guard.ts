6e6cb4cf29c4fc0d4da9813cecef1fb853d0b649
dartungar <dartungar@gmail.com>
2024-10-13 12:03:16 +0400

    Add auth guard & 404 page

--- wishlis-webapp/src/auth/auth.guard.ts
+++ wishlis-webapp/src/auth/auth.guard.ts
@@ -1,5 +1,7 @@
 import { CanActivateFn } from '@angular/router';
+import {AuthService} from "./auth.service";
+import {inject} from "@angular/core";
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  return inject(AuthService).isAuthenticated;
 };
