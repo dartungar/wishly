c7ba7932d92a2eb4b33fc51e776a77ce5e75985b
dartungar <dartungar@gmail.com>
2024-10-13 12:03:16 +0400

    Add auth guard & 404 page

--- wishlis-webapp/src/app/routing.module.ts
+++ wishlis-webapp/src/app/routing.module.ts
@@ -4,18 +4,19 @@ import {SettingsComponent} from "../settings/settings.component";
 import {UserWishlistComponent} from "../wishlist/user-wishlist/user-wishlist.component";
 import {SearchComponent} from "../search/search.component";
 import {AuthFormComponent} from "../auth/auth-form/auth-form.component";
-
-
+import {authGuard} from "../auth/auth.guard";
+import {NotFoundComponent} from "../common/not-found/not-found.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: "/search", pathMatch: 'full' },
   { path: 'search', component: SearchComponent },
   { path: 'user/:id', component: UserWishlistComponent },
-  { path: 'me', component: UserWishlistComponent },
-  { path: 'settings', component: SettingsComponent },
+  { path: 'me', component: UserWishlistComponent, canActivate: [authGuard] },
+  { path: 'settings', component: SettingsComponent, canActivate: [authGuard]  },
   { path: 'auth', component: AuthFormComponent },
   { path: 'auth/sign-in', component: AuthFormComponent },
   { path: 'auth/sign-up', component: AuthFormComponent },
+  { path: '', redirectTo: "/search", pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
