52b7e0b4b74df8400bd1df209f178cc091b42aaa
dartungar <dartungar@gmail.com>
2024-10-17 14:11:57 +0400

    Implement updating the item

--- wishlis-webapp/src/wishlist/wishlist-item/wishlist-item.component.ts
+++ wishlis-webapp/src/wishlist/wishlist-item/wishlist-item.component.ts
@@ -1,13 +1,45 @@
-import {Component, Input} from '@angular/core';
-import {WishlistItem} from "../wishlistItem";
+import {Component, Input, OnInit} from '@angular/core';
+import {createDefaultWishlistItem, WishlistItem} from "../wishlistItem";
+import {NgIf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {WishlistItemsService} from "../wishlist-items.service";
+import {UserService} from "../../user/user.service";
 
 @Component({
   selector: 'app-wishlist-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    FormsModule
+  ],
   templateUrl: './wishlist-item.component.html',
   styleUrl: './wishlist-item.component.css'
 })
-export class WishlistItemComponent {
-  @Input() item: WishlistItem | undefined;
+export class WishlistItemComponent implements OnInit {
+  @Input() authenticatedUserId: string | null;
+  @Input() item: WishlistItem;
+  editing: boolean = false;
+  belongsToAuthenticatedUser = false;
+
+  constructor(private itemsService: WishlistItemsService) {
+  }
+
+  ngOnInit(): void {
+        this.belongsToAuthenticatedUser = (this.authenticatedUserId === this.item.userId);
+        this.item ??= createDefaultWishlistItem(this.authenticatedUserId ?? "");
+    }
+
+  toggleEdit(): void {
+    console.log("toggling edit from: ", this.editing);
+    this.editing = !this.editing;
+    console.log("toggled edit to: ", this.editing);
+  }
+
+  save(): void {
+    this.toggleEdit();
+    this.itemsService.saveItem(this.item).subscribe(x => {
+      console.log("Item saved", x);
+    })
+    // TODO: save via items service
+  }
 }
--- wishlis-webapp/src/wishlist/wishlistItem.ts
+++ wishlis-webapp/src/wishlist/wishlistItem.ts
@@ -1,9 +1,22 @@
 
 export interface WishlistItem {
+  id: string;
   userId: string;
   name: string;
   price: number;
   currency: string;
   url: string;
   isGroupGift: boolean;
+}
+
+export const createDefaultWishlistItem = (userId: string): WishlistItem => {
+  return {
+    id: crypto.randomUUID(),
+    userId,
+    name: "New Item",
+    url: "",
+    price: 0,
+    currency: "",
+    isGroupGift: false
+  }
 }
