2b5d00d246cc15d6b6e803170a2cbc291efb7671
dartungar <dartungar@gmail.com>
2024-12-03 17:37:47 +0400

    Implement search page (design only)

--- wishlis-webapp/src/search/search.component.ts
+++ wishlis-webapp/src/search/search.component.ts
@@ -1,19 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {WishlistItemsService} from "../wishlist/wishlist-items.service";
+import {InputGroupModule} from "primeng/inputgroup";
+import {InputTextModule} from "primeng/inputtext";
+import {ButtonDirective} from "primeng/button";
+import {from, Observable} from "rxjs";
+import {User} from '../user/user';
+import {SearchResultComponent} from "./search-result/search-result.component";
+import {AsyncPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [
+    InputGroupModule,
+    InputTextModule,
+    ButtonDirective,
+    SearchResultComponent,
+    NgForOf,
+    AsyncPipe
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  public searchResults: User[];
 
-  constructor(private itemService: WishlistItemsService) {
+  constructor() {
   }
 
-  getItemsForUser(id: string)  {
-    this.itemService.getItemsForUser(id).subscribe(x => console.log(x));
+  ngOnInit(): void {
+    // TODO: subscribe to search service
+    this.searchResults =
+      [{
+        id: "12345",
+        name: "Test Search Result",
+        currencyCode: "USD",
+        birthday: new Date(),
+        isProfileSearchable: true
+      }];
   }
+
 }
