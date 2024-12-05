0090b760e608d466d36ac8abe7671ad61bf8e8b6
dartungar <dartungar@gmail.com>
2024-12-05 14:05:12 +0400

    Rename search-result component to user-preview

--- wishlis-webapp/src/common/user-preview/user-preview.component.ts
+++ wishlis-webapp/src/common/user-preview/user-preview.component.ts
@@ -3,18 +3,22 @@ import {User} from "../../user/user";
 import {DatePipe} from "@angular/common";
 import {RouterLink} from "@angular/router";
 import {CardModule} from "primeng/card";
+import {UserService} from "../user.service";
 
 @Component({
-  selector: 'app-search-result',
+  selector: 'app-user-preview',
   standalone: true,
   imports: [
     DatePipe,
     RouterLink,
     CardModule
   ],
-  templateUrl: './search-result.component.html',
-  styleUrl: './search-result.component.css'
+  templateUrl: './user-preview.component.html',
+  styleUrl: './user-preview.component.css'
 })
-export class SearchResultComponent {
+export class UserPreviewComponent {
   @Input() user: User;
+
+  constructor(private userService: UserService) {
+  }
 }
