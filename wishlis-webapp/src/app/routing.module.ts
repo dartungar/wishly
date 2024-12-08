3fb6ae435908eb7f3ebdfa44658ec9f8907db19c
dartungar <dartungar@gmail.com>
2024-12-08 16:11:16 +0400

    Improve routes & routing

--- wishlis-webapp/src/app/routing.module.ts
+++ wishlis-webapp/src/app/routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {SettingsComponent} from "../settings/settings.component";
-import {UserWishlistComponent} from "../wishlist/user-wishlist/user-wishlist.component";
-import {SearchComponent} from "../search/search.component";
-import {AuthFormComponent} from "../auth/auth-form/auth-form.component";
-import {authGuard} from "../auth/auth.guard";
-import {NotFoundComponent} from "../common/not-found/not-found.component";
 import {routes} from "./app.routes";
+import {LocationStrategy, PathLocationStrategy} from "@angular/common";
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {bindToComponentInputs: true})],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {
+    bindToComponentInputs: true,
+    onSameUrlNavigation: 'reload',
+    paramsInheritanceStrategy: 'always',
+    urlUpdateStrategy: 'eager'
+  })],
+  exports: [RouterModule],
+  providers: [
+    { provide: LocationStrategy, useClass: PathLocationStrategy }
+  ]
 })
 export class RoutingModule { }
