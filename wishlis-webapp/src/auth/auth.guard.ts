1578bd3cdabce9283613349cb741fb66dee39d30
Danila Nikolaev <danila_nikolaev@epam.com>
2024-12-14 16:26:10 +0400

    Redirect to user's wishlist when authenticated

--- wishlis-webapp/src/auth/auth.guard.ts
+++ wishlis-webapp/src/auth/auth.guard.ts
@@ -1,10 +1,11 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
 import {AuthService} from "./auth.service";
 import {inject} from "@angular/core";
 import {map, take} from "rxjs";
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return inject(AuthService).isAuthenticated$.pipe(take(1),
+  const authService = inject(AuthService);
+  return authService.isAuthenticated$.pipe(take(1),
     map(isAuthenticated => {
       return isAuthenticated;
     }));
