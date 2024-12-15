b5062a2ae8df0b77e6d0f9d047dce9defcd15786
Danila Nikolaev <danila_nikolaev@epam.com>
2024-12-15 18:54:26 +0400

    Improve homepage's design

--- wishlis-webapp/src/home/home.component.ts
+++ wishlis-webapp/src/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {SearchComponent} from "../search/search.component";
+import {Button} from "primeng/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    SearchComponent
-  ],
+    imports: [
+        SearchComponent,
+        Button,
+        RouterLink
+    ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
